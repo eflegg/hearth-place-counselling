f58b75bd587af926e83adefe39ec6ea9db0ebe8a
Erin Flegg <ehflegg@gmail.com>
2022-06-11 14:23:44 -0700

    add fields and render modalities page cards

--- pages/modalities.js
+++ pages/modalities.js
@@ -10,22 +10,42 @@ import { RichText } from "prismic-reactjs";
 import { createClient } from "../prismic";
 import { PrismicRichText, PrismicLink } from "@prismicio/react";
 
-const modalitiesContainer = styled.section``;
+const ModalitiesContainer = styled.div`
+  width: 60%;
+  margin: 0 auto;
+  text-align: center;
+`;
 
-export default function Modalities({ modalities, footer }) {
+const ModalitySingle = styled.div``;
+
+export default function Modalities({ doc, footer }) {
+  const modalities = doc.data;
   return (
     <Layout pageTitle={"Modalities"} footer={footer}>
       <Head>
         <title>Modalities</title>
       </Head>
-      <PrismicRichText field={modalities.data.modalitiesTitle} />
+      <PrismicRichText field={modalities.modalitiesTitle} />
+
+      <ModalitiesContainer className="modalities--container">
+        {modalities.modalities.map((modality, index) => {
+          return (
+            <ModalitySingle key={index} className="modality-card">
+              <PrismicRichText field={modality.title} />
+              <PrismicRichText field={modality.description} />
+              <PrismicRichText field={modality.quote} />
+              <img src={modality.icon.url} alt={modality.icon.alt} />
+            </ModalitySingle>
+          );
+        })}
+      </ModalitiesContainer>
     </Layout>
   );
 }
 
 export async function getStaticProps() {
-  const modalities = await createClient().getSingle("modalities");
+  const doc = await createClient().getSingle("modalities");
   return {
-    props: { modalities },
+    props: { doc },
   };
 }
